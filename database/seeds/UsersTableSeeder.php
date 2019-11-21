b96e550f9fcf4b0b460b71ebdd72f5d25f2fe8d4
mayankpatel104 <mayank.patel104@gmail.com>
2019-11-21 19:33:45 +0530

    new column added

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,21 +13,25 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+
         DB::table('users')->insert([
             'name' => "Mayank Patel",
             'email' => "mayank@yopmail.com",
+            'api_token' => base64_encode('mayank@yopmail.com'),
             'password' => bcrypt('password'),
         ]);
 
         DB::table('users')->insert([
             'name' => "Mayank IT",
             'email' => "mayankp@yopmail.com",
+            'api_token' => base64_encode('mayankp@yopmail.com'),
             'password' => bcrypt('password'),
         ]);
 
         DB::table('users')->insert([
             'name' => "Mayank Solution",
             'email' => "mayankps@yopmail.com",
+            'api_token' => base64_encode('mayankps@yopmail.com'),
             'password' => bcrypt('password'),
         ]);
     }
